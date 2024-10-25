6df9da592c0b9dc4e931f63de185e687db56eeb1
Liam Mefus <liam.mefus@outlook.de>
2024-10-25 16:10:15 +0200

    add(entities): with docker-compose.yaml that contains a maria db for development

---
+++ src/main/java/Entity/DataSetEntry.java
@@ -0,0 +1,73 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "dataset_entry")
+public class DataSetEntry implements Serializable {
+    @Id
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "dataset")
+    private DataSet dataSet;
+    @ManyToOne
+    @JoinColumn(name = "owner")
+    private User owner;
+    @OneToOne
+    private DataSetEntryDetails details;
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedLast;
+
+    public DataSet getDataSet() {
+        return dataSet;
+    }
+
+    public void setDataSet(DataSet dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public DataSetEntryDetails getDetails() {
+        return details;
+    }
+
+    public void setDetails(DataSetEntryDetails details) {
+        this.details = details;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedLast() {
+        return updatedLast;
+    }
+
+    public void setUpdatedLast(LocalDateTime updatedLast) {
+        this.updatedLast = updatedLast;
+    }
+}
---
+++ src/main/java/Entity/DataSetEntryDetails.java
@@ -0,0 +1,53 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "dataset_entry_details")
+public class DataSetEntryDetails implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private DataSetEntry dataSetEntry;
+    @OneToMany
+    private ArrayList<DataBaseEntryChange> dataBaseEntryChanges;
+    private String description;
+    private float emission;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public DataSetEntry getDataSetEntry() {
+        return dataSetEntry;
+    }
+
+    public void setDataSetEntry(DataSetEntry dataSetEntry) {
+        this.dataSetEntry = dataSetEntry;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getEmission() {
+        return emission;
+    }
+
+    public void setEmission(float emission) {
+        this.emission = emission;
+    }
+}

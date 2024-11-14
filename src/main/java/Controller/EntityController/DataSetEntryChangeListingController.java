5ef4e5fe6f295cd94d7bf850d323cb8e837583ee
TrueScare <liam.mefus@outlook.de>
2024-11-14 21:53:11 +0100

    add(entry_change): listing and data query

--- src/main/java/Controller/EntityController/DataSetEntryChangeListingController.java
+++ src/main/java/Controller/EntityController/DataSetEntryChangeListingController.java
@@ -1,12 +1,29 @@
 package Controller.EntityController;
 
+import DAO.DataSetEntryChangeDAO;
 import Entity.DataSetEntry;
+import Entity.DataSetEntryChange;
+import Structs.AppContext;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+
+import java.util.Collection;
 
 @Named
 @RequestScoped
-public class DataSetEntryChangeListingController extends BaseEntityListingController<DataSetEntry> {
+public class DataSetEntryChangeListingController extends BaseEntityListingController<DataSetEntryChange> {
+    @Inject
+    DataSetEntryChangeDAO entityDAO;
+    @Named
+    @Inject
+    private AppContext appContext;
+
     public DataSetEntryChangeListingController() {
+    }
+
+    @Override
+    public Collection<DataSetEntryChange> getListing() {
+        return entityList = entityDAO.getChangesToBeApproved(appContext.getActiveUser().getId());
     }
 }
--- src/main/java/DAO/DataSetEntryChangeDAO.java
+++ src/main/java/DAO/DataSetEntryChangeDAO.java
@@ -1,13 +1,38 @@
 package DAO;
 
+import Entity.DataSetEntry;
 import Entity.DataSetEntryChange;
+import Entity.User;
+import Structs.AppContext;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.persistence.criteria.*;
+
+import java.util.List;
 
 @Named
 @ApplicationScoped
 public class DataSetEntryChangeDAO extends BaseDAO<DataSetEntryChange> {
+
     public DataSetEntryChangeDAO() {
         super(DataSetEntryChange.class);
+    }
+
+    public List<DataSetEntryChange> getChangesToBeApproved(Long userID) {
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<DataSetEntryChange> cq = cb.createQuery(DataSetEntryChange.class);
+        Root<DataSetEntryChange> root = cq.from(DataSetEntryChange.class);
+        cq.select(root);
+        Join<DataSetEntryChange, DataSetEntry> entry = root.join("datasetEntry", JoinType.INNER);
+        Join<DataSetEntry, User> user = entry.join("owner", JoinType.INNER);
+        cq.where(cb.and(
+                        cb.equal(root.get("approved"), false),
+                        cb.equal(user.get("id"), userID)
+                )
+        );
+        cq.orderBy(cb.asc(root.get("createdAt")));
+        return findByCriteria(cq);
     }
 }
